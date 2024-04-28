e814287b065352ecf00fc2abaa0a2d904e5a88f0
Mohammad Zunayed <mdzunayedwd@gmail.com>
2024-04-29 00:36:20 +0600

    find user with email to login

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run() {
         res.send(result);
     });
 
+    app.get('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = {email: email}
+        const cursor = userCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
